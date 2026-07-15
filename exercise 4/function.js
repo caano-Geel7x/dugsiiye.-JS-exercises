e4a30fbeafc2d744fbc1524c9dc6f55be4c219b4
caano-geel7x <zenvision7xgmail.com>
2026-07-15 15:35:37 +0100

    submitted exercise 4

---
+++ exercise 4/function.js
@@ -0,0 +1,19 @@
+
+function calculateTotal(price, quantity) {
+    return price * quantity; 
+}
+
+
+console.log("Total price: $" + calculateTotal(2, 4));  
+console.log("Total price: $" + calculateTotal(5, 8)); 
+
+
+
+
+const calculateTotalExpression = function(price, quantity) {
+    return price * quantity; 
+};
+
+
+console.log("Total price: $" + calculateTotalExpression(6, 3));  
+console.log("Total price: $" + calculateTotalExpression(8, 5)); 
