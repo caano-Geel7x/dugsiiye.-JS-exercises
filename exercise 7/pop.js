d3cfe1c2ac0bb50cfbbbcc3bf5ee1dbdebf26049
caano-geel7x <zenvision7xgmail.com>
2026-07-16 16:30:06 +0100

    submit exercise  7

---
+++ exercise 7/pop.js
@@ -0,0 +1,19 @@
+let colors = ["Black","Green","Yellow",];
+console.log("push");
+colors.push("Red");
+console.log(colors);
+console.log("pop");
+colors.pop();
+console.log(colors);
+console.log("shift");
+colors.shift();
+console.log(colors);
+console.log("unshift");
+colors.unshift("marron");
+console.log(colors);
+
+
+
+ 
+
+
