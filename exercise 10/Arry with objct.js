14cf4077b44c17848bd0ba45be6694977415f2e9
caano-geel7x <zenvision7xgmail.com>
2026-07-20 16:39:24 +0100

    submit exercise 10

---
+++ exercise 10/Arry with objct.js
@@ -0,0 +1,18 @@
+let library = [
+    {
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        year: 1937
+    }, 
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        year: 1960
+    }
+]; 
+
+
+console.log(library[0].title); 
+
+
+console.log(library[1].author);
